28639a165237cd3be26684f6da85a629a02a8618
Денис Мурашов <murashov@zelrun.com>
2022-11-22 14:33:34 +0600

    create users app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('recipes.urls', namespace='recipes')),
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
