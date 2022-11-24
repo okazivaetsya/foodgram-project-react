544aa421f214589324f9de1158cc8fe77a86e7bc
Денис Мурашов <murashov@zelrun.com>
2022-11-24 14:57:58 +0600

    download data to Ingredients model

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
 
 urlpatterns = [
-    path('', include('recipes.urls', namespace='recipes')),
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
 ]
