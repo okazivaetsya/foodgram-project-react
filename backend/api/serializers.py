53cc4c66382b96c6f5a5d240b62619a9ac77c8d2
Денис Мурашов <murashov@zelrun.com>
2022-11-28 14:40:03 +0600

    добавил в выдачу АПИ ингредиенты

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from recipes.models import Recipe, Tag, User
+from recipes.models import Recipe, Tag, User, IngredientsAmount
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -16,15 +16,31 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email', 'id', 'username', 'first_name', 'last_name')
 
 
+class IngredientSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source="ingredient.id")
+    name = serializers.ReadOnlyField(source="ingredient.name")
+    measurement_unit = serializers.ReadOnlyField(
+        source="ingredient.measurement_unit")
+
+    class Meta:
+        model = IngredientsAmount
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     tags = TagSerializer(read_only=True, many=True)
     author = UserSerializer(read_only=True)
+    ingredients = IngredientSerializer(source='ingredients_amount', many=True)
 
     class Meta:
         model = Recipe
         fields = (
-            'id', 'tags',
-            'author', 'name',
-            'image', 'text',
+            'id',
+            'tags',
+            'author',
+            'ingredients',
+            'name',
+            'image',
+            'text',
             'cooking_time'
         )
