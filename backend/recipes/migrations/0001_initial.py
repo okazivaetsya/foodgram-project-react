3f7f0dadc317bfb0508097b4d77c0c36a185cccc
Денис Мурашов <murashov@zelrun.com>
2022-12-20 17:43:30 +0600

    battle commit v1.2

--- backend/recipes/migrations/0001_initial.py
+++ backend/recipes/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Favorites',
+            name='favorites',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
             ],
